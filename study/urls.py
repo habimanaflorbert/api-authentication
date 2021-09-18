15a879ea6b3c91ec9881c108dd11fa17f9984eb5
Florbert 97 <54468365+habimanaflorbert@users.noreply.github.com>
2021-09-18 19:40:10 +0200

    Update urls.py

--- study/urls.py
+++ study/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -23,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 ]
+urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
